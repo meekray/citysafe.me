cbfc034c8efe000b419af17de17056c09f191366
Matthew Chen <mattchen@umich.edu>
2018-08-07 01:03:39 -0700

    Added Splash Screen before React Native runs and a React Native Splash Screen; Updated App icon to logo

--- App.js
+++ App.js
@@ -5,9 +5,13 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './src/components/reducers';
 import RouterComponent from './src/components/RouterComponent';
+import SplashScreen from 'react-native-splash-screen';
 //https://itnext.io/install-react-native-maps-with-gradle-3-on-android-44f91a70a395
 //https://medium.com/@malithjayaweera/adding-a-heatmap-overlay-over-react-native-map-fed02130f2a9
 class App extends Component {
+  componentDidMount(){
+  	SplashScreen.hide();
+  }
   render(){
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
--- android/app/src/main/java/com/safevibes/MainApplication.java
+++ android/app/src/main/java/com/safevibes/MainApplication.java
@@ -3,12 +3,14 @@ package com.safevibes;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.BV.LinearGradient.LinearGradientPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new SplashScreenReactPackage(),
             new MapsPackage(),
             new LinearGradientPackage()
       );
